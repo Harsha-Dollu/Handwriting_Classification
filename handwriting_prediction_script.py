77ecd39e91dee81d8d22fa38ea9e33f96148902f
Dollu Harsha vardhan <163315273+Harsha-Dollu@users.noreply.github.com>
2024-10-11 15:43:41 +0530

    Update handwriting_prediction_script.py

--- handwriting_prediction_script.py
+++ handwriting_prediction_script.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import tensorflow as tf
 
-# Function to preprocess a single image
+
 def preprocess_image(image_path, size=(128, 128)):
     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
     if image is None:
@@ -13,7 +13,7 @@ def preprocess_image(image_path, size=(128, 128)):
     image = np.expand_dims(image, axis=0)   # Add batch dimension
     return image
 
-# Function to predict handwriting quality
+
 def predict_handwriting(image_path, model_path='improved_handwriting_model.keras'):
     model = tf.keras.models.load_model(model_path)
     image = preprocess_image(image_path)
@@ -26,7 +26,7 @@ def predict_handwriting(image_path, model_path='improved_handwriting_model.keras
     
     return label, score
 
-# Test the prediction function
-image_path = r'C:\Users\harsh\OneDrive\Desktop\python\python_project1\download.jpg'  # Replace with your test image path
+
+image_path = r'C:\Users\harsh\OneDrive\Desktop\python\python_project1\download.jpg'  
 result = predict_handwriting(image_path)
 print(f"Prediction: {result[0]}, Actual Score: {result[1]:.4f}")
